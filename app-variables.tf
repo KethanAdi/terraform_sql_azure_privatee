fc15f6f4ea134b8af8be9b5659fda43037a18cc4
KethanAdi <102666849+KethanAdi@users.noreply.github.com>
2022-04-20 17:11:43 +0530

    Update app-variables.tf

--- app-variables.tf
+++ app-variables.tf
@@ -30,7 +30,7 @@ variable "prefix" {
 variable "location" {
   type        = string
   description = "Azure region where the resource group will be created"
-  default     = "North Europe"
+  default     = "West US"
 }
 
 # application or company environment
